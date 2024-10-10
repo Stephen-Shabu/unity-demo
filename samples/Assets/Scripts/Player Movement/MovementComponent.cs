87ad266e61f3121e3a8fb6b5eaec0e86fec9ef8b
Stephen-Shabu <swindle@live.co.uk>
2024-10-10 13:41:33 +0100

    - Added ability to rotate character torward input direction

--- samples/Assets/Scripts/Player Movement/MovementComponent.cs
+++ samples/Assets/Scripts/Player Movement/MovementComponent.cs
@@ -1,15 +1,18 @@
+using UnityEditor;
 using UnityEngine;
 
 public class MovementComponent : MonoBehaviour
 {
     [SerializeField] private Rigidbody attactedRigidBody;
     [SerializeField] private float topSpeed;
-    [SerializeField] private float currentSpeed;
+    [SerializeField] private float topAngularSpeed;
     [SerializeField] private float accelerationFactor;
     [SerializeField] private float deaccelerationFactor;
-    [SerializeField] private float stoppingFactor;
+    [SerializeField] private float angularAccelerationFactor;
     [SerializeField] float inertiaFactor = 5f;
 
+    private float currentTurnSpeed;
+    private float currentLinearSpeed;
     private Vector3 lastMoveVector;
     private Vector3 currentMoveVector;
     private Vector3 finalMoveVector;
@@ -27,35 +30,72 @@ public class MovementComponent : MonoBehaviour
         var targetVelocity = GetMoveVector(direction);
         targetVelocity.y = attactedRigidBody.linearVelocity.y;
         attactedRigidBody.linearVelocity = Vector3.Lerp(attactedRigidBody.linearVelocity, targetVelocity, inertiaFactor * Time.deltaTime);
+        attactedRigidBody.rotation = GetRotation(currentMoveVector);
     }
 
     private Vector3 GetMoveVector(Vector2 direction)
     {
-        var canAccelarate = direction.sqrMagnitude > 0.25f;
-        if (canAccelarate)
+        var canAccelerate = direction.sqrMagnitude > 0.25f;
+        if (canAccelerate)
         {
             lastMoveVector = currentMoveVector;
-            currentMoveVector = direction;
+            currentMoveVector = new Vector3(direction.x, 0, direction.y);
         }
         else
         {
             currentMoveVector = lastMoveVector;
         }
-       
-        var targetSpeed = canAccelarate ? topSpeed : 0;
-        var accelFactor = canAccelarate ? accelerationFactor : deaccelerationFactor;
 
-        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, accelFactor * Time.smoothDeltaTime);
+        currentLinearSpeed += canAccelerate ? accelerationFactor * Time.deltaTime : -(deaccelerationFactor * Time.deltaTime);
 
-        var moveVector = currentMoveVector * currentSpeed;
+        if (currentLinearSpeed > topSpeed)
+        {
+            currentLinearSpeed = topSpeed;
+        }
+        else if (currentLinearSpeed < 0)
+        {
+            currentLinearSpeed = 0;
+        }
+
+        var moveVector = currentMoveVector * currentLinearSpeed;
         finalMoveVector.x = moveVector.x;
-        finalMoveVector.z = moveVector.y;
+        finalMoveVector.z = moveVector.z;
 
-        return finalMoveVector;
+        return moveVector;
     }
 
-    private Quaternion GetRotation()
+    private Quaternion GetRotation(Vector3 direction)
     {
-        return  Quaternion.identity;
+        Vector3 turnDirection = new Vector3(direction.x, 0, direction.z);
+        float forwardTiltAmount = Mathf.Abs(transform.InverseTransformDirection(attactedRigidBody.linearVelocity).z) * 2.5f;
+        float angularTiltAmount = Vector3.Dot(attactedRigidBody.angularVelocity, Vector3.up) * 5f;
+        currentTurnSpeed = topAngularSpeed * Time.smoothDeltaTime;
+
+        float GetAngleFromDirection(Vector3 direction)
+        {
+            return Mathf.Atan2(direction.x, direction.z);
+        }
+
+        float InterpolateAngle(float startAngle, float targetAngle, float t)
+        {
+            var diff = targetAngle - startAngle;
+
+            diff += 2 * Mathf.PI;
+
+            diff = (diff + Mathf.PI) % (2 * Mathf.PI) - Mathf.PI;
+
+            var interpolatedaAngle = startAngle + t * diff;
+
+            return interpolatedaAngle;
+        }
+
+
+        var start = GetAngleFromDirection(transform.forward);
+        var target = GetAngleFromDirection(turnDirection);
+        var angle = InterpolateAngle(start, target, currentTurnSpeed);
+
+        var finalRotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.up);
+
+        return finalRotation;
     }
 }
