9647fab9ac2599302637b7e66938072e6977cae4
Stephen-Shabu <swindle@live.co.uk>
2024-11-30 14:32:51 +0000

    - Updated unity to latest version

--- samples/Assets/Scripts/AI/MobController.cs
+++ samples/Assets/Scripts/AI/MobController.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using static GameEvents.EventsCollection;
+using System;
 
 public class MobController : MonoBehaviour
 {
+    public Action OnHealthReachedZero;
+
     [SerializeField] private Vector2 inputVector = Vector2.zero;
     [SerializeField] private Vector2 lookVector = Vector2.zero;
     [SerializeField] private Vector3 heading = Vector3.zero;
@@ -10,32 +14,48 @@ public class MobController : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private MovementComponent moveComponent;
     [SerializeField] private HealthComponent healthComponent;
+    [SerializeField] private MeleeComponent meleeComponent;
 
     private bool hasHealthReachedZero = false;
+    private bool canLaunchAttack = false;
 
-    private void Start()
+    public void Initialize(Transform newTarget)
     {
         healthComponent.OnHealthReachedZero = HandleOnHealthReachedZero;
+        target = newTarget;
+        Debug.Log("Target Changed");
     }
 
     private void HandleOnHealthReachedZero()
     {
         hasHealthReachedZero = !hasHealthReachedZero;
+        OnHealthReachedZero?.Invoke();
+        meleeComponent.CancelMeleeAttack();
     }
 
     private void FixedUpdate()
     {
-        var moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
-
-        if (!hasHealthReachedZero && Vector3.Distance(transform.position, target.position) > stoppingDistance)
+        if (target != null)
         {
-            heading = target.position - transform.position;
-        }
-        else
-        {
-            heading = Vector3.zero;
-        }
+            var moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
 
-        moveComponent.UpdateMovement(heading.normalized * intensity, false);
+            if (!hasHealthReachedZero && Vector3.Distance(transform.position, target.position) > stoppingDistance)
+            {
+                heading = target.position - transform.position;
+            }
+            else
+            {
+                if (!canLaunchAttack)
+                {
+                    canLaunchAttack = true;
+                    meleeComponent.LaunchMeleeAttack(heading, () => { canLaunchAttack = false; });
+                    Debug.Log("Lauch Attack");
+                }
+
+                heading = Vector3.zero;
+            }
+
+            moveComponent.UpdateMovement(heading.normalized * intensity, false);
+        }
     }
 }
